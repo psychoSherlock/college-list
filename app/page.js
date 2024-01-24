3cc8ab746204f3fd80ede75c556ca4c8379fa43e
Adrin7113 <adrinjosect@gmail.com>
2024-01-24 10:04:50 +0530

    Did filters ui

--- app/page.js
+++ app/page.js
@@ -2,38 +2,9 @@
 import CollegeCard from "@/components/College/CollegeCard";
 import Filters from "@/components/Filters/Filters";
 export default function Home() {
-  const countOpt = [
-    "Australia",
-    "Canada",
-    "China",
-    "Germany",
-    "India",
-    "Ireland",
-    "Malaysia",
-    "Nepal",
-    "New Zealand",
-    "Philippines",
-    "Russia",
-    "Singapore",
-    "South Africa",
-    "Sweden",
-    "Ukraine",
-    "United Kingdom",
-    "United States",
-  ];
-  const levelOpt = [
-    "Diploma",
-    "Bachelor",
-    "Master",
-    "Doctorate",
-    "Certificate",
-  ];
   return (
     <main className="min-h-screen bg-[#003366] flex flex-col items-center p-20">
-      <div className="w-full flex justify-between items-center">
-        <h1 className="mono text-7xl text-[whitesmoke] font-black uppercase">
-          List of colleges
-        </h1>
+      <div className="w-full flex flex-col justify-between items-center gap-10">
         <Filters />
       </div>
       <div className="w-full flex flex-wrap pt-20 mono">
--- components/Filters/Filters.js
+++ components/Filters/Filters.js
@@ -25,17 +25,25 @@ const Filters = () => {
     "Doctorate",
     "Certificate",
   ];
+  const courseOpt = [
+    "Nursing",
+    "Engineering",
+    "Business",
+    "Computer Science",
+    "Medicine",
+    "Psychology",
+  ];
   return (
-    <div className="w-1/2 h-44 bg-[whitesmoke] rounded-lg p-10 flex flex-col gap-5">
-      <div className="flex justify-between">
-        <div className="flex items-center ">
+    <div className="w-2/3  bg-[whitesmoke] rounded-lg p-10 flex flex-col gap-5">
+      <div className="flex justify-between gap-5">
+        <div className="flex items-center w-1/2 justify-between">
           <label htmlFor="countries" className="text-xl mono font-bold">
             Choose a country:
           </label>
           <select
             id="countries"
             name="countries"
-            className="px-4 py-2 ml-5 bg-[#003366] text-white rounded-sm"
+            className="pl-4 py-2 ml-5 w-56 bg-[#003366] text-white rounded-sm"
           >
             {countOpt.map((i, index) => (
               <option value={i} key={index}>
@@ -44,14 +52,14 @@ const Filters = () => {
             ))}
           </select>
         </div>
-        <div className="flex items-center ">
+        <div className="flex items-center w-1/2 justify-between">
           <label htmlFor="level" className="text-xl mono font-bold">
             Choose a level:
           </label>
           <select
             id="level"
             name="level"
-            className="px-4 py-2 ml-5 bg-[#003366] text-white rounded-sm"
+            className="pl-4 py-2 ml-5 w-56 bg-[#003366] text-white rounded-sm"
           >
             {levelOpt.map((i, index) => (
               <option value={i} key={index}>
@@ -60,6 +68,40 @@ const Filters = () => {
             ))}
           </select>
         </div>
+      </div>
+      <div className="flex justify-between gap-5">
+        <div className="flex items-center w-1/2 justify-between">
+          <label htmlFor="types" className="text-xl mono font-bold">
+            Choose a course type:
+          </label>
+          <select
+            id="types"
+            name="types"
+            className="pl-4 py-2 ml-5 w-56 bg-[#003366] text-white rounded-sm"
+          >
+            {courseOpt.map((i, index) => (
+              <option value={i} key={index}>
+                {i}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex items-center w-1/2 justify-between mono">
+          <input
+            name="courseName"
+            type="text"
+            placeholder="Enter the course name"
+            className="p-4 text-black placeholder:text-black w-full border-2 border-[#003366] rounded-md outline-none"
+          />
+        </div>
+      </div>
+      <div className="flex items-center w-full justify-between mono">
+        <input
+          name="collegeName"
+          type="text"
+          placeholder="Enter the college name"
+          className="p-4 text-black placeholder:text-black w-full border-2 border-[#003366] rounded-md outline-none"
+        />
       </div>
     </div>
   );
