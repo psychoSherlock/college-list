2bc5a48f797d601ac21f5c1559cbf3a2dc6ea033
Adrin7113 <adrin7113@gmail.com>
2024-02-04 22:52:26 +0530

    Fixes

--- components/Filters/Filters.js
+++ components/Filters/Filters.js
@@ -11,6 +11,21 @@ const Filters = ({ search, filterOptions }) => {
   return (
     <div className="w-2/3 bg-[whitesmoke] rounded-lg p-10 flex flex-col gap-5">
       <div className="flex flex-col lg:flex-row justify-between gap-5">
+        <div className="flex items-center lg:w-1/2 justify-between mono">
+          <input
+            name="courseName"
+            type="text"
+            placeholder="Course Name"
+            className="p-3 md:p-4 md:text-base text-sm text-black placeholder:text-black w-full border-2 border-[#003366] rounded-md outline-none"
+            value={current.courseName !== "" ? current.courseName : ""}
+            onChange={(e) => {
+              dispatch({
+                type: "CHANGE_FILTER",
+                payload: { ...current, courseName: e.target.value },
+              });
+            }}
+          />
+        </div>
         <div className="flex flex-col gap-2 md:flex-row items-center lg:w-1/2 justify-between">
           <label
             htmlFor="countries"
@@ -34,34 +49,6 @@ const Filters = ({ search, filterOptions }) => {
               filterOptions.uniqueCountries.map((i, index) => (
                 <option value={i} key={index}>
                   {toTitleCase(i)}
-                </option>
-              ))}
-          </select>
-        </div>
-        <div className="flex flex-col gap-2 md:flex-row items-center lg:w-1/2 justify-between">
-          <label
-            htmlFor="level"
-            className="text-xl mono font-bold text-center lg:text-left"
-          >
-            Level:
-          </label>
-          <select
-            id="level"
-            name="level"
-            className="pl-4 py-2 lg:ml-5 w-32 lg:w-44 bg-[#003366] text-white rounded-sm"
-            value={current.program !== "" ? current.program : "Choose"}
-            onChange={(e) => {
-              dispatch({
-                type: "CHANGE_FILTER",
-                payload: { ...current, program: e.target.value },
-              });
-            }}
-          >
-            <option value="">Choose</option>
-            {filterOptions &&
-              filterOptions.uniquePrograms.map((i, index) => (
-                <option value={i} key={index}>
-                  {i}
                 </option>
               ))}
           </select>
@@ -96,20 +83,33 @@ const Filters = ({ search, filterOptions }) => {
               ))}
           </select>
         </div>
-        <div className="flex items-center lg:w-1/2 justify-between mono">
-          <input
-            name="courseName"
-            type="text"
-            placeholder="Course Name"
-            className="p-3 md:p-4 md:text-base text-sm text-black placeholder:text-black w-full border-2 border-[#003366] rounded-md outline-none"
-            value={current.courseName !== "" ? current.courseName : ""}
+        <div className="flex flex-col gap-2 md:flex-row items-center lg:w-1/2 justify-between">
+          <label
+            htmlFor="level"
+            className="text-xl mono font-bold text-center lg:text-left"
+          >
+            Level:
+          </label>
+          <select
+            id="level"
+            name="level"
+            className="pl-4 py-2 lg:ml-5 w-32 lg:w-44 bg-[#003366] text-white rounded-sm"
+            value={current.program !== "" ? current.program : "Choose"}
             onChange={(e) => {
               dispatch({
                 type: "CHANGE_FILTER",
-                payload: { ...current, courseName: e.target.value },
+                payload: { ...current, program: e.target.value },
               });
             }}
-          />
+          >
+            <option value="">Choose</option>
+            {filterOptions &&
+              filterOptions.uniquePrograms.map((i, index) => (
+                <option value={i} key={index}>
+                  {i}
+                </option>
+              ))}
+          </select>
         </div>
       </div>
       <div className="flex flex-col md:flex-row items-center w-full justify-between mono gap-5">
